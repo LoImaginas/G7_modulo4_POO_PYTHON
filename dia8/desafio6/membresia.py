add163b97b69ed660d1d970f5de60cfca74407c8
Lolett Llanquinao <lmarialolett@yahoo.es>
2024-04-17 23:32:28 -0400

    Desafio 6 completado

--- dia8/desafio6/membresia.py
+++ dia8/desafio6/membresia.py
@@ -1,46 +1,74 @@
+"""
+Nombres: 
+-Lolett Llanquinao 
+-Jocelyn Cantero
+-Juan Torres
+-Leonardo Llaupe
+-Claudio Mendez
+Fecha:17-04-2024
+
+Este módulo proporciona una implementación de clases para un sistema de membresía. 
+Incluye una clase abstracta Membresia y sus clases derivadas Gratis, Basica, 
+Familiar, SinConexion y Pro. Cada clase proporciona métodos para cambiar y 
+crear nuevas membresías, así como propiedades para acceder a la información 
+de la membresía actual.
+
+Clases:
+    - Membresia: Clase abstracta que define la estructura básica de una membresía.
+    - Gratis: Clase que representa una membresía gratuita con opciones limitadas.
+    - Basica: Clase que representa una membresía básica con más funciones que Gratis.
+    - Familiar: Clase que representa una membresía para múltiples usuarios.
+    - SinConexion: Clase que representa una membresía con acceso sin conexión.
+    - Pro: Clase que representa una membresía premium con todas las funciones.
+
+Cada clase implementa métodos para cambiar la membresía actual a una diferente.
+
+"""
+
+
 from abc import ABC, abstractmethod
 #constructor
 class Membresia(ABC):
-    def _init_(self, correo_suscriptor: str, numero_tarjeta: str):
-        #atributos de instancia
+    def __init__(self, correo_suscriptor: str, numero_tarjeta: str):
+       # Constructor de la clase Membresia
         self.__correo_suscriptor = correo_suscriptor
         self.__numero_tarjeta = numero_tarjeta
-    #getter 
+    #Getter para el atributo correo_suscriptor
     @property 
     def correo_suscriptor(self):   
         return self.__correo_suscriptor
-
+    # Getter para el atributo numero_tarjeta
     @property
     def numero_tarjeta(self):
         return self.__numero_tarjeta
     
-    #metodo abstracto
+    #metodo abstracto para cambiar la membresia
     @abstractmethod
     def cambiar_membresia(self, tipo: int):
         pass
-
-    @abstractmethod
-    def cancelar_membresia(self, tipo: int):
-        pass 
-
+    #metodo abstracto para crear nueva membresia
     @abstractmethod   
     def crear_nueva_membresia(self, tipo: int):
         pass  
    
-  
-        
+  #metodo para cancelar membresia
+    def cancelar_membresia(self):
+        return Gratis(self.correo_suscriptor, self.numero_tarjeta)
+#clase gratis        
 class Gratis(Membresia):
-    def _init_(self, correo_suscriptor:str, numero_tarjeta:str):
+    def __init__(self, correo_suscriptor:str, numero_tarjeta:str):
+        # Constructor de la clase Gratis
             super().__init__(correo_suscriptor, numero_tarjeta)
             self._costo = 0
             self._max_dispositivos = 1
             
-            # Lógica para cambiar la membresía
+            # Lógica para cambiar la membresía de la clase gratis
     def cambiar_membresia(self, tipo: int):       
         if tipo in range(1, 5):
             return self._crear_nueva_membresia(tipo)
         else:
             return self   
+        # Método para crear una nueva membresía
     def _crear_nueva_membresia(self, tipo: int):
         if tipo == 1:
             return Basica(self.correo_suscriptor, self.numero_tarjeta)
@@ -50,20 +78,25 @@ class Gratis(Membresia):
             return SinConexion(self.correo_suscriptor, self.numero_tarjeta)
         elif tipo == 4:
             return Pro(self.correo_suscriptor, self.numero_tarjeta)
-#1
+      # Método necesario para implementar en la clase abstracta Membresia
+    def crear_nueva_membresia(self, tipo: int):
+        return self._crear_nueva_membresia(tipo)   
+        
+#1 Clase Basica
 class Basica(Membresia):
     def __init__(self, correo_suscriptor: str, numero_tarjeta: str):
+        #Constructor de la clase Basica
         super().__init__(correo_suscriptor, numero_tarjeta)
         self._costo = 3000
         self._max_dispositivos = 2
         
-    # Lógica para cambiar la membresía
+    # Lógica para cambiar la membresía de la clase basica
     def cambiar_membresia(self, tipo: int):
         if tipo in range(2, 4):
             return self._crear_nueva_membresia(tipo)
         else:
             return self
-        
+    # Método para crear una nueva membresía  
     def _crear_nueva_membresia(self, tipo: int):
         if tipo == 2:
             return Familiar(self.correo_suscriptor, self.numero_tarjeta)
@@ -71,65 +104,99 @@ class Basica(Membresia):
             return SinConexion(self.correo_suscriptor, self.numero_tarjeta)
         elif tipo == 4:
             return Pro(self.correo_suscriptor, self.numero_tarjeta)        
+
+    # Implementación del método abstracto
+    def crear_nueva_membresia(self, tipo: int):
+        return self._crear_nueva_membresia(tipo)  
         
-#2
+#2 Clase Familiar
 class Familiar(Basica):
-    
-    
-    
-    
-    
+    def __init__(self, correo_suscriptor: str, numero_tarjeta: str):
+         #Constructor de la clase Familiar
+        super().__init__(correo_suscriptor, numero_tarjeta)
+        self._costo = 5000
+        self._max_dispositivos = 5
+        self._dias_regalo = 7
+        
+        # Lógica para cambiar la membresía de la clase familiar
     def cambiar_membresia(self, tipo: int):
-        if tipo in [1, 3, 4]:
-            # Lógica para cambiar la membresía
-            pass
+        if tipo in [1, 3, 4]:   
+            return self._crear_nueva_membresia(tipo) 
         else:
             return self
+    # Método para crear una nueva membresía
+    def _crear_nueva_membresia(self, tipo: int):
+        if tipo == 1:
+            return Basica(self.correo_suscriptor, self.numero_tarjeta)
+        elif tipo == 3:
+            return SinConexion(self.correo_suscriptor, self.numero_tarjeta)
+        elif tipo == 4:
+            return Pro(self.correo_suscriptor, self.numero_tarjeta)
 
-    def modificar_control_parental(self):
         # Lógica para modificar el control parental
+    def modificar_control_parental(self):
         pass
-#3
+    
+#3 Clase SinConexion
 class SinConexion(Basica):
+    def __init__(self, correo_suscriptor: str, numero_tarjeta: str):
+         #Constructor de la clase SinConexion
+        super().__init__(correo_suscriptor, numero_tarjeta)
+        self._costo = 3500
+        self._max_dispositivos = 2
+        self._dias_regalo = 7
     
-    
-    
-    
-    
+     # Lógica para cambiar la membresía
     def cambiar_membresia(self, tipo: int):
         if tipo in [1, 2, 4]:
-            # Lógica para cambiar la membresía
-            pass
+            return self._crear_nueva_membresia(tipo)
         else:
-            return self
+            return self    
+        # Método para crear una nueva membresía
+    def _crear_nueva_membresia(self, tipo: int):
+        if tipo == 1:
+            return Basica(self.correo_suscriptor, self.numero_tarjeta)
+        elif tipo == 2:
+            return Familiar(self.correo_suscriptor, self.numero_tarjeta)
+        elif tipo == 4:
+            return Pro(self.correo_suscriptor, self.numero_tarjeta)
 
+       # Lógica para aumentar el contenido sin conexión
     def aumentar_contenido_sin_conexion(self):
-        # Lógica para aumentar el contenido sin conexión
         pass
-#4
+#4 Clase Pro
 class Pro(Familiar, SinConexion):
+    def __init__(self, correo_suscriptor: str, numero_tarjeta: str):
+         #Constructor de la clase Pro
+        super().__init__(correo_suscriptor, numero_tarjeta)
+        self._costo = 7000
+        self._max_dispositivos = 6
+        self._dias_regalo = 15
     
-    
-    
-    
-    
+    # Lógica para cambiar la membresía
     def cambiar_membresia(self, tipo: int):
-        if 1 <= tipo <= 3:
-            # Lógica para cambiar la membresía
-            pass
+        if tipo in range(1, 3):
+            return self._crear_nueva_membresia(tipo)   
         else:
             return self
+    # Método para crear una nueva membresía 
+    def _crear_nueva_membresia(self, tipo: int):
+        if tipo == 1:
+            return Basica(self.correo_suscriptor, self.numero_tarjeta)
+        elif tipo == 2:
+            return Familiar(self.correo_suscriptor, self.numero_tarjeta)
+        elif tipo == 3:
+            return SinConexion(self.correo_suscriptor, self.numero_tarjeta)    
 
+    # Lógica para modificar el control parental
     def modificar_control_parental(self):
-        # Lógica para modificar el control parental
         pass
-
+    # Lógica para aumentar el contenido sin conexión
     def aumentar_contenido_sin_conexion(self):
-        # Lógica para aumentar el contenido sin conexión
         pass
 
 # Ejemplo de uso
-g = Gratis("correo@prueba.cl", "123 456 789")
+g = Gratis("correo@prueba.cl", "123456789")
 print(type(g))
 b = g.cambiar_membresia(1)
 print(type(b))
